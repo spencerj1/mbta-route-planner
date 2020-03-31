1400a2183b23fa9d90b957b60a5f7ec86637e5f1
James Spencer <jamesspencer348@gmail.com>
2020-03-31 16:58:11 -0400

    adding comments to example 3

--- js/example3.js
+++ js/example3.js
@@ -1,12 +1,13 @@
   let template
   let allRouteStops
   let allIntersectingStops
+  // Fetch the Handlebars template to be used
   $.ajax({
     url: '/example3Template',
     cache: true,
     success: function(data) {
         const source = data
-        template = Handlebars.compile(source) //Using Handlebars as the templating engine
+        template = Handlebars.compile(source)
     }               
   })
 
@@ -17,15 +18,18 @@
     success: function(stopList) {
         console.log(stopList)
         allRouteStops = stopList
+        // Add each of the Line options to the line selectors
         $.each(stopList, function (index, value) {
         	$(`<option>${index}</option>`).appendTo('#startLineSelector')
         	$(`<option>${index}</option>`).appendTo('#destinationLineSelector')
         })
+        // Add each of the Station options to the station selectors
         populateSelector($('#startLineSelector option:selected').text(), '#startStationSelector')
         populateSelector($('#destinationLineSelector option:selected').text(), '#destinationStationSelector')
     }               
   })
 
+  // Function for populating the station selectors when given line is changed
   function populateSelector(line, selector) {
   	$(selector).empty()
   	$.each(allRouteStops[line], function (index, value) {
@@ -33,6 +37,7 @@
   	})
   }
 
+  // Attach change listener to line selectors
   $('#startLineSelector').on('change', function() {
   	populateSelector(this.value, '#startStationSelector')
   });
@@ -41,21 +46,42 @@
   	populateSelector(this.value, '#destinationStationSelector')
   });
 
+  // The function for calculating the Route given starting and ending stations
   function calculateRoute(callback) {
+  	// Collect values from the form fields
   	const startLine = $('#startLineSelector option:selected').text()
     const endLine = $('#destinationLineSelector option:selected').text()
     const startStation = $('#startStationSelector option:selected').text()
     const endStation = $('#destinationStationSelector option:selected').text()
+
+    // Push each calculated station into this array - function will eventually return this as the solution
     let route = []
-    // Fetch the list of intersecting stops from API
+
+    /* Fetch the list of intersecting stops from API
+    	returns intersectingStops JSON - here's and example of the data model:
+    		{
+    			"Station": {
+    				"lines": ['Line Name', 'Line Name', 'Line Name'],
+    			},
+    			"Station": {
+    				"lines": ['Line Name', 'Line Name'],
+    			}
+    		}
+    */
     $.ajax({
       url: '/example2/getIntersectingStops',
       cache: true,
       success: function(intersectingStops) {
+      	// Once the API has returned list of intersectingStops, run our algorithm
+      	// Always push the initial start point to the solution route array
       	route.push({[startLine]: startStation})
+
+      	// Check to see if stations are on the same line
         if(startLine != endLine){
+        	// Loop through intersectingStops list to find the first stop which intersects the End Line
         	for (const station in intersectingStops){
         		let lines = intersectingStops[station].lines
+        		// If the stop includes both the Start Line and End Line, push both into the route array and return
         		if(lines.includes(startLine) && lines.includes(endLine)){
         			route.push({[endLine]: station})
         			route.push({[endLine]: endStation})
@@ -63,16 +89,24 @@
         			return
         		}
         	}
+        	// If the previous loop did not return, than the route requires more than one Line transfer
+        	// Loop through the intersectingStops list again to find stops with a common Line
         	if(route.length == 1){
         		for (const station in intersectingStops){
         			let lines = intersectingStops[station].lines
+        			// If we find an intersectingStation that includes our start Line, loop through the other
+        			// 'connector' lines in the Station.lines array
         			if(lines.includes(startLine)){
         				for(const line in lines){
+        				// For each line (minus the start line) in station.lines array, loop back
+        				// through intersectingStops list to find another station containing the 'connector' line we are searching for
         					let connectingLine = lines[line]
         					if(connectingLine != startLine){
         					for(const station2 in intersectingStops){
         						if (intersectingStops[station2].lines.includes(connectingLine)
         					 	&& intersectingStops[station2].lines.includes(endLine)){
+        					 		// If we find a intersectingStop that includes our 'connector' line, as well as our end Line,
+        					 		// push the connecting stations/lines, then the end station/ end Line, into the route array and return
         							route.push({[connectingLine]: station})
         							if(station2 != endStation){
         								route.push({[endLine]: station2})
@@ -88,6 +122,8 @@
         	}
             }
         }
+        // If stations are on the same line, just push the Line and End Station into 
+        // the route array and return
         route.push({[endLine]: endStation})
         callback(route)
         return
@@ -98,6 +134,8 @@
    function displayRoute() {
    	 $('#routeList').empty()
    	 const route = calculateRoute(function(route){
+   	 	// For each element in the solution route array, compile a handlebars template
+   	 	// and inject into the #routeList container
    	 	$.each(route, function(index, value) {
    	 		let routeJson = {}
    	 		if (index == 0){
